9e2d94f08bd36a1a06dd7aaa00335d1eadaad3b6
Mike Lima <mike.rodrigues.lima@gmail.com>
2024-12-17 21:41:40 -0300

    Go (Golang) Tutorial #4 - Printing & Formatting Strings

---
+++ printing.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	//string
+	var name string = "Hello, \n"
+
+	fmt.Print(name)
+
+	fmt.Println("skip")
+	fmt.Println("line")
+
+	age := 20
+	myName := "Mike"
+
+	fmt.Println("my age is", age, "and my name is", myName)
+
+	fmt.Printf("my age is %v and my name is %v\n", age, myName)
+	fmt.Printf("my age is %q and my name is %q\n", age, myName)
+	fmt.Printf("age is of type %T\n", age)
+	fmt.Printf("name is of type %T\n", name)
+	fmt.Printf("you score %0.3f points\n", 19.89)
+}
--- variables.go
+++ variables.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func variables() {
 	//string
 	var name string = "Mario"
 	var name2 = "Luigi"
