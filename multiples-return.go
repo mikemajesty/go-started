99e31f804a4e09f5745519d5450141f87295013b
Mike Lima <mike.rodrigues.lima@gmail.com>
2024-12-18 17:11:55 -0300

    Go (Golang) Tutorial #10 - Multiple Return Values

---
+++ multiples-return.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func getInitials(name string) (string, string) {
+	s := strings.ToUpper(name)
+	names := strings.Split(s, " ")
+
+	var initials []string
+
+	for _, v := range names {
+		initials = append(initials, v[:1])
+	}
+
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+
+	return initials[0], "_"
+}
+
+func main() {
+	fn, sn := getInitials("mike lima")
+	fmt.Println(fn, sn)
+
+	fn2, sn2 := getInitials("mike")
+	fmt.Println(fn2, sn2)
+}
