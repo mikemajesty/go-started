5acfba15c0f1486cc0c08262d7517f252cd5cd6d
Mike Lima <mike.rodrigues.lima@gmail.com>
2024-12-18 17:28:26 -0300

    Go (Golang) Tutorial #11 - Package Scope

---
+++ package-scope-hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var points = []int{20, 90, 100, 45, 70}
+
+func sayNewHello(name string) {
+	fmt.Println("Hello", name)
+}
---
+++ package-scope.go
@@ -0,0 +1,5 @@
+package main
+
+func packageScope() {
+	sayNewHello("Mike")
+}
