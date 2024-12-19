96388ccbc31df21a85fba806aa829a62db0a5952
Mike Lima <mike.rodrigues.lima@gmail.com>
2024-12-19 16:15:18 -0300

    Go (Golang) Tutorial #18 - User Input

--- receiver-function.go
+++ receiver-function.go
@@ -1,10 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
-func receiverFunction() {
-	myBill := newFuncionBill("maario's bill")
-	myBill.updateTip(10)
-	myBill.addItem("onion ring", 5.21)
-	fmt.Println(myBill.fomat())
+func getInput(prompt string, read *bufio.Reader) (string, error) {
+	fmt.Println(prompt)
+	input, err := read.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
+func createBill() billFunction {
+	reader := bufio.NewReader(os.Stdin)
+
+	name, _ := getInput("Enter the bill name: ", reader)
+
+	bill := newFuncionBill(name)
+	fmt.Println("Created the bill -", bill.name)
+	return bill
+}
+
+func promptOpt(bill billFunction) {
+	reader := bufio.NewReader(os.Stdin)
+
+	opt, _ := getInput("Choose options (a - add item, s - save bill, t - add tip) ", reader)
+	fmt.Println("Options -", opt)
+}
+
+func main() {
+	myBill := createBill()
+	promptOpt(myBill)
+	fmt.Println(myBill)
 }
